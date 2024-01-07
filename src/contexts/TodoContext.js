209406d475b081d01ab3eace2cba89858cb5acaf
Hotdesk Laptop Surface 3 <hotdesk001@onmicrosoft.com>
2024-01-07 20:22:39 +1100

    updated TodoContext file

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -8,7 +8,7 @@ const TodoContextProvider = (props) => {
     // state of todos, which is an array of todo_object with 3 properties: name, category, id
     const [ todos,  dispatch ] = useReducer(todoReducer, [], () => {
         const localData = localStorage.getItem('todos');
-        return localData ? localData.parse(localData) : [];
+        return localData ? JSON.parse(localData) : [];
     });
 
     useEffect(() => {
